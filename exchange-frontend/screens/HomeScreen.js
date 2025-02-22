614ebd8364c5ff6822e30e198ef5d1c5ff1d6e87
Lucas <luc.coulson6@gmail.com>
2025-02-22 12:26:33 -0500

    Static page with everything on it
    spacing is good added border radius to smooth
    everything out and shadows. Uhhh so like I
    didn't know you can do negative margins thats
    why I've been stuck for so long.

--- exchange-frontend/screens/HomeScreen.js
+++ exchange-frontend/screens/HomeScreen.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import globalStyles from '../styles/globalStyles';
 
 const HomeScreen = () => {
   return (
     <>
-    <View style={globalStyles.container}>
-        <View>
-            <Text style={styles.title}>Exchange</Text>
-        </View>
-        <View style={styles.image}>
-            <Image
-            source ={{ uri: 'https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png', }} 
-            style={{ width: 350, height: 200, borderWidth: "5", borderRadius: '5', overflow: 'hidden',}}
-            resizeMode="contain" />
+      <View style={styles.container}>
+          <View>
+              <Text style={styles.title}>Exchange</Text>
+          </View>
+          <View style={styles.image}>
+              <Image
+              source ={{ uri: 'https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png', }} 
+              style={styles.picture}
+              resizeMode="contain" />
+          </View>
+          <View style={styles.background}>
+            <Text style={styles.description}>Description</Text>
+            <Text style={{ fontSize: 20, marginLeft: 10,}}>alksdjfhalskdjfhalskdjfhalsdjfkhasl</Text>
+
             
-        </View>
-        
-    </View>
+          </View>
+      
+          
+      </View>
+      
     </>
   );
 };
@@ -25,16 +32,51 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F6DCAC',
+    backgroundColor: "#F6DCAC",
+    
+  },
+  background: {
+    flex: 1,
+    marginLeft: 20,
+    marginTop: -180,
+    width: 350,
+    justifyContent: 'flex-start',
+    alignItems: 'left',
+    backgroundColor: "#FBEED6",
+    borderRadius: 20,
+    overflow: 'hidden',
+    shadowCOlor: "#000",
+    shadowRadius: 4,
+    
+  },
+
+  description: {
+    marginLeft: 10,
+    marginBottom: 20,
+    marginTop: 20,
+    justifyContent: "flex-start",
+    textAlign: 'left',
+    fontSize: 35,
+    fontWeight: 'bold',
+    
   },
   image: {
-    marginTop: '50',
+    marginTop: 50,
     flex: 1,
     alignItems: "center",
  
   },
+  picture: {
+    width: 350,
+    height: 200,
+    borderRadius: 25,
+    overflow: 'hidden',
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+
+  },
+
   titleContainer: {
     flex: 1,
     justifyContent: 'flex-start',
