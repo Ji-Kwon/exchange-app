243eaf96a2c8cf5ad51510ac62139bb8813bc13b
kruugerr <zachgould2004@gmail.com>
2025-02-22 11:57:41 -0500

    updated message controller

--- exchange-backend/controllers/message.controller.js
+++ exchange-backend/controllers/message.controller.js
@@ -29,4 +29,39 @@ exports.createMessage = async(req, res) => {
     } catch(error){
         res.status(500).json({ error: error.message });
     }
-}
+};
+
+exports.updateMessage = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [updated] = await Message.update(req.body, {
+        where: { message_id: id }
+      });
+  
+      if (updated) {
+        const updatedMessage = await Message.findByPk(id);
+        return res.status(200).json(updatedMessage);
+      }
+  
+      throw new Error('Message not found');
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  exports.deleteMessage = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleted = await Message.destroy({
+        where: { message_id: id }
+      });
+  
+      if (deleted) {
+        return res.status(204).send();
+      }
+  
+      throw new Error('Message not found');
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
