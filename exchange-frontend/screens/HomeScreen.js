8c6e23cd12bd1c51aa5f3382370194e673542442
Lucas <luc.coulson6@gmail.com>
2025-02-23 05:14:01 -0500

    You can now swipe on images
    used a list instead of importing functions

--- exchange-frontend/screens/HomeScreen.js
+++ exchange-frontend/screens/HomeScreen.js
@@ -1,59 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, FlatList, Dimensions } from 'react-native';
 import ListLabels from '../components/ListLabels';
 
+const windowWidth = Dimensions.get('window').width;
 
-
+const images = [
+  { 
+    source: require('../assets/images/favourites.png'),
+    dimensions: { width: 350, height: 300 }
+  },
+  {
+    uri: 'https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png',
+    title: 'Image 1',
+  },
+  {
+    uri: 'https://i.imgflip.com/1hnv2v.jpg?a483384',
+    title: 'Image 2',
+  },
+];
 
 const HomeScreen = () => {
+  const renderItem = ({ item }) => (
+    <View style={styles.picture}>
+      {item.source ? (
+        <Image source={item.source} style={styles.flatListImage} resizeMode="cover" />
+      ) : (
+        <Image source={{ uri: item.uri }} style={styles.flatListImage} resizeMode="cover" />
+      )}
+    </View>
+  );
 
   return (
-    
     <>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <View style={styles.container}>
-            
-              <View>
-                  <Text style={styles.title}>Exchange</Text>
-              </View>
-              <View style={styles.image}>
-                  <Image
-                  source ={{ uri: 'https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png', }} 
-                  style={styles.picture}
-                  resizeMode="contain" />
-              </View>
-              <View style={styles.background}>
-                <Text style={styles.description}>Description</Text>
-                <Text style={{ fontSize: 20, marginLeft: 10,}}>alksdjfhalskdjfhalskdjfhalsdjfkhasl</Text>
-              </View>
-
-              <View style={styles.exchange}>
-                  <Text style={styles.exchangeHeading}>Exchange Offered</Text>
-                  <ListLabels/>
-                  <View>
-                      
-                  </View>
-              </View>
-
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.container}>
+          
+          <View>
+            <Text style={styles.title}>Exchange</Text>
           </View>
-        </ScrollView>
+
+          <View style={styles.image}>
+            <FlatList
+              data={images}
+              renderItem={renderItem}
+              keyExtractor={(item, index) => index.toString()}
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+
+          <View style={styles.background}>
+            <Text style={styles.description}>Description</Text>
+            <Text style={{ fontSize: 20, marginLeft: 10 }}>alksdjfhalskdjfhalskdjfhalsdjfkhasl</Text>
+          </View>
+
+          <View style={styles.exchange}>
+            <Text style={styles.exchangeHeading}>Exchange Offered</Text>
+            <ListLabels />
+          </View>
+
+        </View>
+      </ScrollView>
     </>
   );
 };
-/*
-The labels are supposed to be imported in the <View style exchange />.
-*/
 
 const styles = StyleSheet.create({
   scrollContainer: {
-   
-   
+    flexGrow: 1,
   },
   container: {
     paddingBottom: 20,
     alignItems: "center",
     backgroundColor: "#F6DCAC",
-    
   },
   background: {
     marginVertical: 20,
@@ -61,7 +81,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FBEED6",
     borderRadius: 20,
     overflow: 'hidden',
-    shadowColor: "#000",         // Fixed typo
+    shadowColor: "#000",
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 5,
@@ -74,7 +94,7 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',  // Aligns to the left
   },
   exchangeHeading: {
-    textAlign: 'left',        // Centers text within container
+    textAlign: 'left',        
     fontSize: 35,
     fontWeight: 'bold',
   },
@@ -87,14 +107,10 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     fontSize: 35,
     fontWeight: 'bold',
-    
   },
   image: {
     marginTop: 50,
     alignItems: "center",
- 
-  },
-  picture: {
     width: 350,
     height: 200,
     borderRadius: 25,
@@ -102,20 +118,23 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOpacity: 0.2,
     shadowRadius: 4,
-
+    backgroundColor: 'black',
   },
-
-  titleContainer: {
-    
-
+  picture: {
+    width: 350,
+    height: 200,
+    borderRadius: 25,
+    overflow: 'hidden',
+  },
+  flatListImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 25,
   },
   title: {
     marginTop: 50,
     marginRight: 120,
-    
-    fontFamily: 'SuperMagic-L3XVn', // not working
     fontSize: 50,
-
   },
 });
 
